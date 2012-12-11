1fa8b2b93a8204970ad68bb0285aeabde164bb78
starkingwx <starkingwx@gmail.com>
2012-12-11 14:41:20 +0800

    add number filter

--- src/com/richitec/imeeting/contactselect/ContactSelectActivity.java
+++ src/com/richitec/imeeting/contactselect/ContactSelectActivity.java
@@ -1447,7 +1447,7 @@ public class ContactSelectActivity extends IMeetingNavigationActivity {
 			public void onClick(View v) {
 				// get phone button text
 				String _selectedPhone = (String) ((Button) v).getText();
-
+				_selectedPhone = AddressBookManager.filterNumber(_selectedPhone);
 				// dismiss contact phone select popup window
 				dismiss();
 
@@ -1468,7 +1468,7 @@ public class ContactSelectActivity extends IMeetingNavigationActivity {
 					int position, long id) {
 				// get phone listView item data
 				String _selectedPhone = (String) ((TextView) view).getText();
-
+				_selectedPhone = AddressBookManager.filterNumber(_selectedPhone);
 				// dismiss contact phone select popup window
 				dismiss();
 
